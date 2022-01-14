d1aa45337e67b46161560fb50a0dd4c6a79f96ba
ugvignon <Ugo.VIGNON@etu.uca.fr>
2022-01-14 15:12:54 +0100

    persistance du ScoreRanking fonctionnel (en passant par un ScoreRankingSerializable

--- code/src/model/Serialization/ScoreRankingSerializable.java
+++ code/src/model/Serialization/ScoreRankingSerializable.java
@@ -1,14 +1,16 @@
 package model.Serialization;
 
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScoreRankingSerializable {
+public class ScoreRankingSerializable implements Serializable {
 
-    private final List<StateSerializable> list = new ArrayList<>();
+    private  static  final  long serialVersionUID =  1350092881346723535L;
+    private final List<StateSerializable> ranking = new ArrayList<>();
 
-    public List<StateSerializable> getList() {
-        return list;
+    public List<StateSerializable> getRanking() {
+        return ranking;
     }
 }
--- code/src/model/Serialization/StateSerializable.java
+++ code/src/model/Serialization/StateSerializable.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class StateSerializable implements Serializable {
 
-    private  static  final  long serialVersionUID =  1350092881346723535L;
     private int level;
     private int score;
     private int time;
